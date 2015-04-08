14e80a15aa5ecf4689a41ba7a8456df78d78789b
Ale <Ale@ale_pc>
2015-04-08 18:47:22 -0500

    modificaciones ale

--- gamcultt/entidades/src/main/java/mx/ipn/escom/entidades/Casa.java
+++ gamcultt/entidades/src/main/java/mx/ipn/escom/entidades/Casa.java
@@ -1,7 +1,5 @@
 package mx.ipn.escom.entidades;
-// Generated 8/04/2015 05:34:35 PM by Hibernate Tools 4.3.1
-
-
+// Generated 8/04/2015 05:34:35 PM by Hibernate Tools 4.3.1      
 
 
 import java.util.List;
